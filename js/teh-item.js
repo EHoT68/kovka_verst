bb83689aa7350de2237692f9a1f4f08b344c2ca3
ehot <tytytyty444478@gmail.com>
2022-01-31 13:19:40 +0300

    ver.4

---
+++ js/teh-item.js
@@ -0,0 +1,66 @@
+function toggleColor1() {
+
+   var myButtonClasses = document.getElementById("btm-teh1").classList;
+  
+   if (myButtonClasses.contains("teh-item-100")) {
+      myButtonClasses.remove("teh-item-100");
+  
+   } else {
+      myButtonClasses.add("teh-item-100");
+   }
+}
+function toggleColor2() {
+
+   var myButtonClasses = document.getElementById("btm-teh2").classList;
+  
+   if (myButtonClasses.contains("teh-item-100")) {
+      myButtonClasses.remove("teh-item-100");
+  
+   } else {
+      myButtonClasses.add("teh-item-100");
+   }
+}
+function toggleColor3() {
+
+   var myButtonClasses = document.getElementById("btm-teh3").classList;
+  
+   if (myButtonClasses.contains("teh-item-100")) {
+      myButtonClasses.remove("teh-item-100");
+  
+   } else {
+      myButtonClasses.add("teh-item-100");
+   }
+}
+function toggleColor4() {
+
+   var myButtonClasses = document.getElementById("btm-teh4").classList;
+  
+   if (myButtonClasses.contains("teh-item-100")) {
+      myButtonClasses.remove("teh-item-100");
+  
+   } else {
+      myButtonClasses.add("teh-item-100");
+   }
+}
+function toggleColor5() {
+
+   var myButtonClasses = document.getElementById("btm-teh5").classList;
+  
+   if (myButtonClasses.contains("teh-item-100")) {
+      myButtonClasses.remove("teh-item-100");
+  
+   } else {
+      myButtonClasses.add("teh-item-100");
+   }
+}
+function toggleColor6() {
+
+   var myButtonClasses = document.getElementById("btm-teh6").classList;
+  
+   if (myButtonClasses.contains("teh-item-100")) {
+      myButtonClasses.remove("teh-item-100");
+  
+   } else {
+      myButtonClasses.add("teh-item-100");
+   }
+}
